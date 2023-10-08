83afcdde55ba7c76c65a923880867ab9769ff9f0
Majd <Majd.fahadd@gmail.com>
2023-10-08 10:31:31 +0300

    Initial Commit

---
+++ Majd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Majd
+//
+//  Created by Majd Almutairi on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Majd/MajdApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajdApp.swift
+//  Majd
+//
+//  Created by Majd Almutairi on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MajdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
